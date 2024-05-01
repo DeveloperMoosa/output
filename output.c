86f165f392e1cdc6252b3a1b769410897ece462d
DeveloperMoosa <161608892+DeveloperMoosa@users.noreply.github.com>
2024-05-01 22:59:03 +0500

    Add files via upload

---
+++ output.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int age = 23;
+    float pi = 3.14;  // the value of pi is in the form of decimal point
+    char hashtag = '#';  // character is always written in single quote
+
+// Getting the output of given variable
+
+printf("the age is %d \n", age);
+printf("the value is %f \n", pi);
+printf("the character is %c \n", hashtag);
+return 0;
+}
